be579327acba75f94cb946445472947ea4736a6d
sathyainfotech <84429523+sathyainfotech@users.noreply.github.com>
2021-06-07 21:57:41 -0700

    File Upload

---
+++ Main.py
@@ -0,0 +1,11 @@
+from Module import *
+
+import Module
+
+Myname=name
+print(Myname)
+
+fun()
+
+obj=Bio("Sandy")
+obj.data("Sathya","Salem","sathya@gmail.com")
---
+++ Module.py
@@ -0,0 +1,10 @@
+name="Sathya"
+
+def fun():
+    print("My Function")
+
+class Bio:
+    def __init__(self,name):
+        print("Hi...",name)
+    def data(self,name,address,mail):
+        print("\n",name,"\n",address,"\n",mail)
